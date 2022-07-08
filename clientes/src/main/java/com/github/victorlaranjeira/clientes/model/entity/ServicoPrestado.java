46fe2ea14353aa47cda873d93c51d8b301e86600
Victor Laranjeira <victor-laranjeira18@hotmail.com>
2022-07-08 17:13:53 -0300

    Adição API ServicoPrestadoController

--- clientes/src/main/java/com/github/victorlaranjeira/clientes/model/entity/ServicoPrestado.java
+++ clientes/src/main/java/com/github/victorlaranjeira/clientes/model/entity/ServicoPrestado.java
@@ -1,13 +1,15 @@
 package com.github.victorlaranjeira.clientes.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Data
-public class Servico {
+public class ServicoPrestado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +22,11 @@ public class Servico {
     @JoinColumn(name = "id_cliente")
     private Cliente cliente;
 
+    @Column
     private BigDecimal valor;
+
+    @Column
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
 
 }
--- clientes/src/main/java/com/github/victorlaranjeira/clientes/model/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.victorlaranjeira.clientes.model.repository;
-
-import com.github.victorlaranjeira.clientes.model.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
